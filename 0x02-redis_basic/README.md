f15aa03d47157a07571683ae92ee726184c48cfa
Vivian <okpala_vivian@yahoo.com>
2024-07-17 16:34:30 +0000

    Task 0

---
+++ 0x02-redis_basic/README.md
@@ -0,0 +1,1 @@
+#Solutions to the 0x02-redis_basic project
---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Module Docs"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    def __init__(self):
+        """Initializes a Redit client"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Takes a data argument and returns a string"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
