1eda94f2180426835896a1d2827dee7ecc2c2639
Vivian <okpala_vivian@yahoo.com>
2024-07-17 16:44:38 +0000

    Task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Module Docs"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -16,3 +16,23 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) ->
+            Union[str, bytes, int, float, None]:
+        """
+        Take a key string argument and an optional Callable argument named fn
+        """
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> Optional[str]:
+        """Function Docs"""
+        return self.get(key, fn=lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """Function Docs"""
+        return self.get(key, fn=int)
